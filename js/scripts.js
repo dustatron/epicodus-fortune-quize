b25ff3e5a2acb584f8e12cf2d88df04414b5a804
Dusty McCord and Nick Gallo <nick@>
2020-01-22 14:18:44 -0800

    wire up some boxes and the survey button. Then print them to the DOM.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,33 @@
-$(document).ready(function(){
+$(document).ready(function () {
+    
+    //Submit button function
+    $('form#fortune').submit(function (event) {
+        event.preventDefault();
+        
+            var badThings = [];
+            var goodThings = [];
+
+        $('input:checkbox[name=unlucky]:checked').each(function () {
+            var selectedOption = $(this).val();
+            badThings.push(selectedOption);
+        });
+
+        $('input:checkbox[name=good]:checked').each(function () {
+            var selectedOption = $(this).val();
+            goodThings.push(selectedOption);
+        });
+
+        $('.result').empty();
+        printOutArray(badThings);
+        printOutArray(goodThings);
+    });
+
+    //Write to page
+    function printOutArray(printThis) {
+        var result = $('.result');
+        printThis.forEach(function (item) {
+            result.append('<li>' + item + '</li>');
+        });
+    }
 
 });
