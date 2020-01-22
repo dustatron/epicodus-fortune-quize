ebe48b16ecef57590e78388e15ed77ec4198cd37
Dusty McCord and Nick Gallo <dusty@>
2020-01-22 15:03:39 -0800

    made fortune eval score.

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,56 @@
 $(document).ready(function () {
+    var total;
     
     //Submit button function
     $('form#fortune').submit(function (event) {
         event.preventDefault();
         
-            var badThings = [];
-            var goodThings = [];
+        var badThings = [];
+        var goodThings = [];
 
         $('input:checkbox[name=unlucky]:checked').each(function () {
-            var selectedOption = $(this).val();
+            var selectedOption = parseInt($(this).val());
             badThings.push(selectedOption);
         });
 
         $('input:checkbox[name=good]:checked').each(function () {
-            var selectedOption = $(this).val();
+            var selectedOption = parseInt($(this).val());
             goodThings.push(selectedOption);
         });
 
+        if(badThings.length > 0 && goodThings.length > 0){
+            total = (badThings.reduce(function(a,b){return a + b})) - (goodThings.reduce(function(a,b){return a + b}));
+        } else if (badThings.length === 0 && goodThings.length >= 1 ){
+            total = 0;
+        } else if (badThings.length >= 1 && goodThings.length === 0) {
+            total = badThings.reduce(function(a,b){return a + b});
+        } else {
+            total = 10000;
+        }
+
+        
+        console.log(total);
         $('.result').empty();
-        printOutArray(badThings);
-        printOutArray(goodThings);
+
+
+        if (total <= 100) {
+            printResult("You will get a coding job")
+        } else if (total > 101 && total < 250) {
+            printResult("your mother will learn to make soup, better.")
+        } else if(total > 250){
+            printResult("You will win a perfectly adequate pen.")
+        } else {
+            printResult("fail");
+        };
+
+        // printOutArray(badThings);
+        // printOutArray(goodThings);
+
     });
 
-    //Write to page
+
+
+    //Write to page Functions
     function printOutArray(printThis) {
         var result = $('.result');
         printThis.forEach(function (item) {
@@ -30,4 +58,17 @@ $(document).ready(function () {
         });
     }
 
+    function printResult(item) {
+        var result = $('.result');
+        result.append('<li>' + item + '</li>');
+    } 
+
+
 });
+
+
+/*fortune options:
+1. You will get a coding job. = 250 >
+2. your mother will learn to make soup, better.= 250 - 100
+3. You will win a perfectly adequate pen = 100 <
+*/
